281023d190742478c3a3e508f02e9794f214292f
Cherie Menchaca <menchaca.cherie@student.greenriver.edu>
2022-05-17 12:57:22 -0400

    modified pets to class

--- index.php
+++ index.php
@@ -1,24 +1,36 @@
 <?php
 
-    //Turn on error reporting
+//Turn on error reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-//Require the autoload file
-require_once ('vendor/autoload.php');
+require('pet.php');
 
-//Create an instance of the Base class
-$f3 = Base::instance();
+$pet = new Pet("Rover", "brown");
+$pet2 = new Pet("Chachi", "purple");
+$pet3 = new Pet("pink");
+$pet->eat();
+$pet2->eat();
+$pet3->eat();
+$pet->sleeping();
+$pet->talking();
 
-//Define a default route
-$f3->route('GET /', function () {
-//    echo '<h1>Pet home</h1>';
 
-    $view = new Template();
-    echo $view->render('views/home.html');
-    }
-
-);
-
-//Run fat free
-$f3-> run();
+////Require the autoload file
+//require_once ('vendor/autoload.php');
+//
+////Create an instance of the Base class
+//$f3 = Base::instance();
+//
+////Define a default route
+//$f3->route('GET /', function () {
+////    echo '<h1>Pet home</h1>';
+//
+//    $view = new Template();
+//    echo $view->render('views/pet-home.html');
+//    }
+//
+//);
+//
+////Run fat free
+//$f3-> run();
